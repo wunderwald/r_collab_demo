f067be1b351e4690d4cc68abe9eb712a875892ab
Moritz Wunderwald <moritzwunderwald@yahoo.de>
2025-11-30 11:56:44 +0100

    implemented plotting

--- Main.R
+++ Main.R
@@ -5,7 +5,7 @@ library(XLConnect)
 parse_ibi <- function(path) {
   ibi <- readWorksheet(loadWorkbook(path),sheet=1, header=FALSE)
   colnames(ibi) <- c("IBI")
-  return(ibi)
+  return(ibi$IBI)
 }
 
 # parse IBI data
@@ -15,3 +15,7 @@ ibi_b <- parse_ibi("HRV_b.xlsx")
 # TODO: calculate summary stats for IBI series
 
 # TODO: plot IBI series
+t <- 1:min(length(ibi_a), length(ibi_b))
+
+plot(t, ibi_a[t], type = "l", col = "blue", lwd = 2, xlab = "Beat index", ylab = "IBI (ms)", main = "Two IBI Signals")
+lines(t, ibi_b[t], col = "red", lwd = 2)
